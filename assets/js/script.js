47d13bf29838e5f7961958dfd32c9a124d4d8f92
Nisha Bukke <sononenisha827@gmail.com>
2024-09-11 14:05:19 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,3 +63,18 @@ function displaySection(evt, id) {
 }
 
 
+// loader js
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+          "body").style.visibility = "hidden";
+      document.querySelector(
+          "#preloader").style.visibility = "visible";
+  } else {
+      document.querySelector(
+          "#preloader").style.display = "none";
+      document.querySelector(
+          "body").style.visibility = "visible";
+  }
+  };
